da27f87ec3e9dfb9de8f37818c223a7558cf1336
dominicbutler <dominic.butler@exonar.com>
2021-03-13 12:17:16 +0000

    basic api interaction

---
+++ app/javascript/apiRequests.tsx
@@ -0,0 +1,60 @@
+import axios from 'axios';
+
+// posts
+
+export const getPosts = () => {
+    return axios
+        .get('/api/posts')
+        .then(res => res.data)
+        .catch(err => console.log(err));
+};
+
+export const createPost = (body, userId) => {
+    const payload = { post: { body: body, user_id: userId } };
+    return axios
+        .post('/api/posts', payload)
+        .then(res => res.data)
+        .catch(err => console.log(err));
+};
+
+// users
+
+export const getUsers = () => {
+    return axios
+        .get('/api/users')
+        .then(res => res.data)
+        .catch(err => console.log(err));
+};
+
+export const createUser = (firstname, lastname) => {
+    const payload = {
+        firstname: firstname,
+        lastname: lastname,
+    };
+    return axios
+        .post('/api/users', payload)
+        .then(res => res.data)
+        .catch(err => console.log(err));
+};
+
+export const deleteUser = id => {
+    return axios
+        .delete('/api/users/' + id)
+        .then(res => res.data)
+        .catch(err => console.log(err));
+};
+
+// images
+
+export const uploadPostImage = id => {
+    var formData = new FormData();
+    var imagefile = document.querySelector('#file');
+    formData.append('image', imagefile.files[0]);
+    const config = {
+        headers: { 'content-type': 'multipart/form-data' },
+    };
+    return axios
+        .post(`/api/posts/${id}/image`, formData, config)
+        .then(res => res.data)
+        .catch(err => console.log(err));
+};
--- app/javascript/components/pages/HelloWorld.tsx
+++ app/javascript/components/pages/HelloWorld.tsx
@@ -1,14 +1,56 @@
-import React from "react";
-import ApplicationLayout from "../layouts/ApplicationLayout";
+import React, { useState, useEffect } from 'react';
+import ApplicationLayout from '../layouts/ApplicationLayout';
+import * as api from '../../apiRequests';
 
 const HelloWorld = () => {
-  return (
-    <ApplicationLayout>
-      <div className="container py-5">
-        <h1>Hello world!!!!</h1>
-      </div>
-    </ApplicationLayout>
-  );
-}
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        api.getPosts().then(d => {
+            console.log(d);
+            setPosts(d.posts);
+        });
+    }, []);
+
+    return (
+        <ApplicationLayout>
+            <div className='container py-5'>
+                {/* <button
+                    onClick={() => {
+                        api.getUsers().then(d => console.log(d));
+                    }}
+                >
+                    get users
+                </button>
+                <button onClick={() => api.createUser('dom', 'butler')}>
+                    create user
+                </button>
+                <button onClick={() => api.deleteUser(1)}>delete user</button> */}
+                <button onClick={() => api.createPost('this is a post', 10)}>
+                    create post
+                </button>
+                <form
+                    id='uploadForm'
+                    onSubmit={e => {
+                        e.preventDefault();
+                        api.uploadPostImage(1);
+                    }}
+                    role='form'
+                    method='post'
+                >
+                    <input type='file' id='file' name='file' />
+                    <input type='submit' value='Upload' />
+                </form>
+            </div>
+            <div>
+                {posts.map((post, i) => (
+                    <div key={i}>
+                        <img src={post.image_url} style={{ width: 100 }} />
+                        {post.body}
+                    </div>
+                ))}
+            </div>
+        </ApplicationLayout>
+    );
+};
 
 export default HelloWorld;
